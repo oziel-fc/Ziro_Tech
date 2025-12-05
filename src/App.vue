131ce98b8b706964a9ac4df2328861b9d78e7acf
oziel-fc <ozielfc.oficial@gmail.com>
2025-12-04 21:16:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import Header from './components/Header/Header.vue'
+</script>
+
+<template>
+  <Header>Teste</Header>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div :class="$style.container">
+      <a :class="$style.link_logo" href="/">
+        <img :id="$style.logo" src="../../../public/ziro_logo.png" alt="logo">
+      </a>
+    </div>
+  </header>
+</template>
+
+<style module>
+  header {
+    width: 100%;
+    height: 105px;
+    background-color: black;
+    display: flex;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+  }
+  .container {
+    width: var(--default-page-width);
+    height: 100%;
+    display: flex;
+    position: relative;
+    align-items: end;
+    padding-bottom: 12px;
+  }
+  .link_logo {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    margin-left: 10px;
+  }
+  #logo {
+    height: 32px;
+  }
+</style>
