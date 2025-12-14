b89dabfb1c2add03d1e57d81bf8364e0ad95e226
oziel-fc <ozielfc.oficial@gmail.com>
2025-12-14 02:41:12 -0300

    Finishing the transition function between slides

--- src/components/MainPage/Carousel.vue
+++ src/components/MainPage/Carousel.vue
@@ -1,5 +1,6 @@
 <script setup>
-    import { ref } from 'vue'
+    import { transform } from 'typescript'
+    import { ref, onMounted } from 'vue'
 
     const carousel_items = [{
             img_path: "/src/assets/carousel/phone_headset.png",
@@ -18,21 +19,22 @@
         }
     ]
     
-    let selected_img = ref(1)
+    let currentBanner = ref(1)
+    const lenBanners = carousel_items.length
+    const currentScreenWidth = window.innerWidth
+    console.log(currentScreenWidth)
 
     const changeBanner = (step) => {
-        const len = carousel_items.length
-
         if (step == 1) {
-            selected_img.value += step
-            if (selected_img.value > len) {
-                selected_img.value = 1
+            currentBanner.value += step
+            if (currentBanner.value > lenBanners) {
+                currentBanner.value = 1
             }
         }
         else if (step == -1) {
-            selected_img.value -= 1
-            if (selected_img.value < 1) {
-                selected_img.value = len
+            currentBanner.value -= 1
+            if (currentBanner.value < 1) {
+                currentBanner.value = lenBanners
             }
         }
     }
@@ -43,7 +45,8 @@
   <div :class="$style.carousel">
     
 
-    <div :class="$style.carousel_track">
+    <div :class="$style.carousel_slides"
+    :style="{width: `calc(100% * ${lenBanners})`, transform: `translateX(calc(${currentScreenWidth}px * -${currentBanner - 1}))`}">
         <div
             v-for="item in carousel_items"
             :class="$style.banner_image">
@@ -68,9 +71,9 @@
         <!-- Indicators are created based on the number of images -->
         <div
             v-for="i in carousel_items.length"
-            :class="[$style.indicators_bars, selected_img === i ? $style.active : '']"
+            :class="[$style.indicators_bars, currentBanner === i ? $style.active : '']"
             :key="i"
-            @click="selected_img = i">
+            @click="currentBanner = i">
         </div>
     </div>
 
@@ -80,21 +83,27 @@
 <style module>
     .carousel {
         position: relative;
-        background-color: aquamarine;
         width: 100%;
         aspect-ratio: 5.56 / 1;
+    }
+    .carousel_slides {
+        height: 100%;
+        overflow: hidden;
+        position: relative;
+        z-index: 1;
+        display: flex;
+        flex-direction: row;
+        transition: transform 0.8s cubic-bezier(0.25, 0.8, 0.25, 1);
+    }
+    .banner_image {
+        height: 100%;
+        width: 100%;
     }
     .carousel img {
         width: 100%;
         height: 100%;
     }
-    .carousel_track {
-        height: 100%;
-        overflow: hidden;
-        position: relative;
-        width: 100%;
-        z-index: 1;
-    }
+
     .navigators {
         align-items: center;
         display: flex;
@@ -141,7 +150,7 @@
         border-radius: 50%;
         border: #ffffff solid 1px;
         cursor: pointer;
-        transition: background-color 0.5s ease, transform 0.5s ease;
+        transition: background-color 0.8s ease, transform 0.8s ease;
     }
     .active {
         background-color: #ffffff;
